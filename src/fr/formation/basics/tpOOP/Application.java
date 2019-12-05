1b5524b026352df7337377910e1e1973917a4211
Yannis <yannis.samba@gmail.com>
2019-12-05 16:32:47 +0100

    indent

--- src/fr/formation/basics/tpOOP/Application.java
+++ src/fr/formation/basics/tpOOP/Application.java
@@ -7,13 +7,13 @@ import java.util.Random;
 public class Application {
 
     public static void main(String[] args) {
-	Cowboy luckyLuke = createLuckyLuke();
-	Cowboy joeDalton = createJoeDalton();
-	System.out.println("Before shooting:");
-	print(luckyLuke, joeDalton);
-	shootUntilDeath(luckyLuke, joeDalton);
-	System.out.println("After shooting:");
-	print(luckyLuke, joeDalton);
+        Cowboy luckyLuke = createLuckyLuke();
+        Cowboy joeDalton = createJoeDalton();
+        System.out.println("Before shooting:");
+        print(luckyLuke, joeDalton);
+        shootUntilDeath(luckyLuke, joeDalton);
+        System.out.println("After shooting:");
+        print(luckyLuke, joeDalton);
     }
 
     /**
@@ -24,34 +24,34 @@ public class Application {
      * aller au plus simple.
      */
     private static void shootUntilDeath(Cowboy luckyLuke, Cowboy joeDalton) {
-	// To be implemented
-		int firstShooter;
-		int lastShooter;
-		Random random = new Random();
-		Cowboy[] cowboys = {luckyLuke, joeDalton};
-		while(true) {
-			firstShooter = random.nextInt(2);
-			lastShooter = Math.abs(firstShooter - 1);
-			cowboys[firstShooter].shoot(cowboys[lastShooter]);
-			if (cowboys[lastShooter].isDead()) {
-				break;
-			}
-		}
-	}
+        // To be implemented
+        int firstShooter;
+        int lastShooter;
+        Random random = new Random();
+        Cowboy[] cowboys = {luckyLuke, joeDalton};
+        while (true) {
+            firstShooter = random.nextInt(2);
+            lastShooter = Math.abs(firstShooter - 1);
+            cowboys[firstShooter].shoot(cowboys[lastShooter]);
+            if (cowboys[lastShooter].isDead()) {
+                break;
+            }
+        }
+    }
 
     private static Cowboy createLuckyLuke() {
-	// To be implemented
-	return new Cowboy("Lucky LUKE",10 ,new Weapon("Colt 5",5,2));
+        // To be implemented
+        return new Cowboy("Lucky LUKE", 10, new Weapon("Colt 5", 5, 2));
     }
 
     private static Cowboy createJoeDalton() {
-	// To be implemented
-		return new Cowboy("Joe DALTON",10 ,new Weapon("Colt 7",7,1));
+        // To be implemented
+        return new Cowboy("Joe DALTON", 10, new Weapon("Colt 7", 7, 1));
     }
 
     private static void print(Cowboy luckyLuke, Cowboy joeDalton) {
-	System.out.println(luckyLuke);
-	System.out.println(joeDalton);
-	System.out.println();
+        System.out.println(luckyLuke);
+        System.out.println(joeDalton);
+        System.out.println();
     }
 }
--- src/fr/formation/basics/tpalgo1/Application.java
+++ src/fr/formation/basics/tpalgo1/Application.java
@@ -4,29 +4,35 @@ import com.sun.java.accessibility.util.TopLevelWindowListener;
 
 public class Application {
 
-    /** Do not change. */
-    private final static String[] TOP = { "╔═", "══", "╗" };
+    /**
+     * Do not change.
+     */
+    private final static String[] TOP = {"╔═", "══", "╗"};
 
-    /** Do not change. */
-    private final static String[] INTERMEDIATE = { "║ ", "O ", "║" };
+    /**
+     * Do not change.
+     */
+    private final static String[] INTERMEDIATE = {"║ ", "O ", "║"};
 
-    /** Do not change. */
-    private final static String[] BOTTOM = { "╚═", "══", "╝" };
+    /**
+     * Do not change.
+     */
+    private final static String[] BOTTOM = {"╚═", "══", "╝"};
 
     /**
      * Main method, do not change but the brick's dimensions.
      */
     public static void main(String[] args) {
-	// Hard-coded example of a 2x4 brick
-	System.out.println("Hard-coded example:");
-	System.out.println("╔═════════╗");
-	System.out.println("║ O O O O ║");
-	System.out.println("║ O O O O ║");
-	System.out.println("╚═════════╝");
-	System.out.println("Brain-coded example:");
-	// Can change to test other dimensions
-	LegoBrick brick = new LegoBrick(2, 4);
-	displayBrick(brick);
+        // Hard-coded example of a 2x4 brick
+        System.out.println("Hard-coded example:");
+        System.out.println("╔═════════╗");
+        System.out.println("║ O O O O ║");
+        System.out.println("║ O O O O ║");
+        System.out.println("╚═════════╝");
+        System.out.println("Brain-coded example:");
+        // Can change to test other dimensions
+        LegoBrick brick = new LegoBrick(2, 4);
+        displayBrick(brick);
     }
 
     /**
@@ -41,35 +47,28 @@ public class Application {
      * Make use of the 3 arrays declared as constants in this class.
      */
     private static void displayBrick(LegoBrick brick) {
-	StringBuilder sb = new StringBuilder();
-	    for(int i=0; i<brick.getHeight()+2; i++){
-	        for(int j=0; j<brick.getWidth()+2; j++){
-	            if(j == 0 && i == 0){
-	                sb.append(TOP[0]);
-                }
-	            else if(j == brick.getWidth()+1 && i == 0){
-	                sb.append(TOP[2]);
-	                sb.append("\n");
-                }
-                else if(j == 0 && i == brick.getHeight()+1){
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < brick.getHeight() + 2; i++) {
+            for (int j = 0; j < brick.getWidth() + 2; j++) {
+                if (j == 0 && i == 0) {
+                    sb.append(TOP[0]);
+                } else if (j == brick.getWidth() + 1 && i == 0) {
+                    sb.append(TOP[2]);
+                    sb.append("\n");
+                } else if (j == 0 && i == brick.getHeight() + 1) {
                     sb.append(BOTTOM[0]);
-                }
-                else if(j == brick.getWidth()+1 && i == brick.getHeight()+1){
+                } else if (j == brick.getWidth() + 1 && i == brick.getHeight() + 1) {
                     sb.append(BOTTOM[2]);
                     sb.append("\n");
-                }
-                else if(i == 0 || i == brick.getHeight()+1){
-	                sb.append(TOP[1]);
-                }
-	            else if(j == 0 ){
-	                sb.append(INTERMEDIATE[0]);
-                }
-	            else if(j == brick.getWidth()+1){
+                } else if (i == 0 || i == brick.getHeight() + 1) {
+                    sb.append(TOP[1]);
+                } else if (j == 0) {
+                    sb.append(INTERMEDIATE[0]);
+                } else if (j == brick.getWidth() + 1) {
                     sb.append(INTERMEDIATE[0]);
                     sb.append("\n");
-                }
-	            else{
-	                sb.append(INTERMEDIATE[1]);
+                } else {
+                    sb.append(INTERMEDIATE[1]);
                 }
             }
         }
